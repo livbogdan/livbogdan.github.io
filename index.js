a9c13502bec0a58b17d228937d7c6a53d73c5358
livbogdan <livbogdan@gmail.com>
2022-09-22 20:15:43 +0200

    V0.4
    Added:
    -Dropdpwn in header for my 3 games
    -New CSS script for Games pages
    -Mapp to evrey my projects (games)
    -Media for every games (screenshots)
    -html scripts for Kubikos and Alcotraz games
    -Footer
    
    Changes:
    -Header now with Dropdown
    -Slideshow Gallery -> Carousel
    -Return contact form tab to header
    
    Removed:
    -Portfolio scripts both HTML and CSS

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
 let slideIndex = 1;
-
-window.onload =function()
+window.onload = function()
 {
-    showSlides(slideIndex);
+  showSlides(slideIndex);
 }
 
-
-function plusSlides(n) 
-{
+// Next/previous controls
+function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-function currentSlide(n) 
-{
+// Thumbnail image controls
+function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-function showSlides(n) 
-{
+function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) 
-    {
-    slideIndex = 1
-    }
-    if (n < 1) 
-    {
-    slideIndex = slides.length
-    }
-  for (i = 0; i < slides.length; i++)
-{
+  let slides = document.getElementsByClassName("screenshots");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
-}
-  for (i = 0; i < dots.length; i++) 
-  {
+  }
+  for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-
-  slides[slideIndex-1].style.display="block";
+  slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
+function headerFunction() 
+{
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") 
+    {
+      x.className += " responsive";
+    } 
+    else 
+    {
+      x.className = "topnav";
+    }
 }
