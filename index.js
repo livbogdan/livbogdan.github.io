eb987b4489e1cb3b4f9f16f904f45b6cdda35b1e
livbogdan <livbogdan@gmail.com>
2022-09-24 18:12:18 +0200

    V0.4.1
    Added:
    Conuntdown timer to Alcotraz page

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//#region Slideshow
 let slideIndex = 1;
 window.onload = function()
 {
@@ -29,8 +30,9 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex+1].className += " active";
 }
+//#endregion
 
-
+//#region NavigationBar
 function headerFunction() 
 {
     var x = document.getElementById("myTopnav");
@@ -43,3 +45,37 @@ function headerFunction()
       x.className = "topnav";
     }
 }
+//#endregion
+
+//#region Countdown
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 7, 2023 00:00:00").getTime();
+
+// Update the count down every 1 second
+var countdownfunction = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+  
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+  
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+  // Output the result in an element with id="alcotraz"
+  document.getElementById("alcotraz").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(countdownfunction);
+    document.getElementById("alcotraz").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+//#endregion
